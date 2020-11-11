84d0f4de54bfd7b9b2458b0127b1257b8a51c3da
Jay <jaykamas@gmail.com>
2020-11-11 16:45:21 -0500

    Added Blank Contact Card

---
+++ src/components/BlankContactCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="contact-card card">
+        <h2>No Contacts!</h2>
+        <h4>Add a friend in Google Contacts</h4>
+        <h4>Make sure Chrome Sync is enabled</h4>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "BlankContactCard"
+}
+</script>
+
+<style>
+.blank-card {
+    height: 200px;
+    text-align: center;
+}
+
+</style>
--- src/components/ContactCardContainer.vue
+++ src/components/ContactCardContainer.vue
@@ -1,21 +1,23 @@
 <template>
-    <div v-if="listOfContacts.length > 0" id="cards-container">
+    <div v-if="listOfContacts.length > 0" class="cards-container">
         <div v-for="singleContact in listOfContacts" :key="singleContact">
             <ContactCard :contact="singleContact"/>
         </div>
     </div>
-    <div v-else>
-        <h2>No contacts to display!</h2>
+    <div v-else class="cards-container">
+        <BlankContactCard />
     </div>
 </template>
 
 <script>
 import ContactCard from '@/components/ContactCard.vue'
+import BlankContactCard from '@/components/BlankContactCard.vue'
+
 
 
 export default {
     name: "ContactCardContainer",
-    components: { ContactCard  },
+    components: { ContactCard, BlankContactCard },
     props: {
         listOfContacts: Array,
     }
@@ -23,7 +25,7 @@ export default {
 </script>
 
 <style>
-#cards-container {
+.cards-container {
     height: 350px;
     overflow: overlay;
     padding-right: 8px;
